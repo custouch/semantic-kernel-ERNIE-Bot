65f227d24b44f1bcd58d2b6364ff80791b594fc4
xbotter <xbotter@live.cn>
2023-06-26 10:01:48 +0800

    add todo

---
+++ src/Connectors.AI.ERNIE-Bot/ERNIEBotChatResult.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.SemanticKernel.AI.ChatCompletion;
+using Microsoft.SemanticKernel.AI.TextCompletion;
+using Microsoft.SemanticKernel.Orchestration;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Connectors.AI.ERNIEBot
+{
+    internal class ERNIEBotChatResult : IChatStreamingResult, ITextStreamingResult
+    {
+        public ModelResult ModelResult => throw new NotImplementedException();
+
+        public Task<ChatMessageBase> GetChatMessageAsync(CancellationToken cancellationToken = default)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<string> GetCompletionAsync(CancellationToken cancellationToken = default)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IAsyncEnumerable<string> GetCompletionStreamingAsync(CancellationToken cancellationToken = default)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IAsyncEnumerable<ChatMessageBase> GetStreamingChatMessageAsync(CancellationToken cancellationToken = default)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
