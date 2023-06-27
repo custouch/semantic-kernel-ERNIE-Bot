a17de6dc21538c1bf592aac436df52fa80002543
xbotter <xbotter@live.cn>
2023-06-27 12:30:05 +0800

    update sample

--- samples/SK-ERNIE-Bot.Sample/Controllers/ApiController.cs
+++ samples/SK-ERNIE-Bot.Sample/Controllers/ApiController.cs
@@ -2,7 +2,10 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.SemanticKernel;
 using Microsoft.SemanticKernel.AI.ChatCompletion;
+using Microsoft.SemanticKernel.AI.Embeddings;
+using Microsoft.SemanticKernel.AI.TextCompletion;
 using SK_ERNIE_Bot.Sample.Controllers.Models;
+using System.Text;
 
 namespace SK_ERNIE_Bot.Sample.Controllers
 {
@@ -33,6 +36,61 @@ namespace SK_ERNIE_Bot.Sample.Controllers
 
             return Ok(result);
 
+        }
+
+        [HttpPost("text")]
+        public async Task<IActionResult> TextAsync([FromBody] UserInput input, CancellationToken cancellationToken)
+        {
+            if (string.IsNullOrWhiteSpace(input.Text))
+            {
+                return NoContent();
+            }
+
+            var completion = _kernel.GetService<ITextCompletion>();
+
+            var result = await completion.GetCompletionsAsync(input.Text, new CompleteRequestSettings(), cancellationToken: cancellationToken);
+
+            var text = await result.First().GetCompletionAsync();
+            return Ok(text);
+
+        }
+
+        [HttpPost("stream")]
+        public async Task ChatStreamAsync([FromBody] UserInput input, CancellationToken cancellationToken)
+        {
+            if (string.IsNullOrWhiteSpace(input.Text))
+            {
+                await Response.CompleteAsync();
+            }
+
+            var chat = _kernel.GetService<IChatCompletion>();
+            var history = chat.CreateNewChat();
+            history.AddUserMessage(input.Text);
+
+            var results = chat.GenerateMessageStreamAsync(history, cancellationToken: cancellationToken);
+
+            await foreach (var result in results)
+            {
+                await Response.WriteAsync("data: " + result + "\n\n", Encoding.UTF8);
+                await Response.Body.FlushAsync();
+            }
+
+            await Response.CompleteAsync();
+        }
+
+        [HttpPost("embedding")]
+        public async Task<IActionResult> EmbeddingAsync([FromBody] UserInput input, CancellationToken cancellationToken)
+        {
+            if (string.IsNullOrWhiteSpace(input.Text))
+            {
+                return NoContent();
+            }
+
+            var embedding = _kernel.GetService<ITextEmbeddingGeneration>();
+
+            var result = await embedding.GenerateEmbeddingAsync(input.Text, cancellationToken);
+
+            return Ok(result);
         }
     }
 }
--- src/Connectors.AI.ERNIE-Bot/ERNIEBotKernelBuilderExtensions.cs
+++ src/Connectors.AI.ERNIE-Bot/ERNIEBotKernelBuilderExtensions.cs
@@ -3,6 +3,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.SemanticKernel.AI.ChatCompletion;
 using Microsoft.SemanticKernel.AI.Embeddings;
+using Microsoft.SemanticKernel.AI.TextCompletion;
 
 namespace Microsoft.SemanticKernel
 {
@@ -10,21 +11,37 @@ namespace Microsoft.SemanticKernel
     {
         public static KernelBuilder WithERNIEBotChatCompletionService(this KernelBuilder builder,
             IServiceProvider service, IConfiguration configuration,
-            string? serviceId = null, bool setAsDefault = false)
+            string? serviceId = null,
+            bool alsoAsTextCompletion = true,
+            bool setAsDefault = false)
         {
             var client = CreateERNIEBotClient(service, configuration);
             var generation = new ERNIEBotChatCompletion(client);
             builder.WithAIService<IChatCompletion>(serviceId, generation, setAsDefault);
+
+            if (alsoAsTextCompletion)
+            {
+                builder.WithAIService<ITextCompletion>(serviceId, generation, setAsDefault);
+            }
+
             return builder;
         }
 
         public static KernelBuilder WithERNIEBotTurboChatCompletionService(this KernelBuilder builder,
             IServiceProvider service, IConfiguration configuration,
-            string? serviceId = null, bool setAsDefault = false)
+            string? serviceId = null,
+            bool alsoAsTextCompletion = true,
+            bool setAsDefault = false)
         {
             var client = CreateERNIEBotClient(service, configuration);
             var generation = new ERNIEBotTurboChatCompletion(client);
             builder.WithAIService<IChatCompletion>(serviceId, generation, setAsDefault);
+
+            if (alsoAsTextCompletion)
+            {
+                builder.WithAIService<ITextCompletion>(serviceId, generation, setAsDefault);
+            }
+
             return builder;
         }
 
