ad350eb65ae964eef8b7a7d9ceca02e2189bd83a
xbotter <xbotter@live.cn>
2023-10-30 19:41:58 +0800

    MemoryClientBuilder -> KernelMemoryBuilder

--- src/ERNIE-Bot.KernelMemory/KernelMemoryBuilderExtensions.cs
+++ src/ERNIE-Bot.KernelMemory/KernelMemoryBuilderExtensions.cs
@@ -9,17 +9,17 @@ using System.Threading.Tasks;
 namespace ERNIE_Bot.KernelMemory
 {
     /// <summary>
-    /// Provides extension methods for configuring the MemoryClientBuilder with ERNIEBot defaults.
+    /// Provides extension methods for configuring the KernelMemoryBuilder with ERNIEBot defaults.
     /// </summary>
-    public static class MemoryClientBuilderExtensions
+    public static class KernelMemoryBuilderExtensions
     {
         /// <summary>
-        /// Configures the MemoryClientBuilder with ERNIEBot text generation.
+        /// Configures the KernelMemoryBuilder with ERNIEBot text generation.
         /// </summary>
-        /// <param name="builder">The MemoryClientBuilder instance to configure.</param>
+        /// <param name="builder">The KernelMemoryBuilder instance to configure.</param>
         /// <param name="client">The ERNIEBotClient instance to use for text generation.</param>
         /// <param name="endpoint">The endpoint to use for the text generation model. Defaults to ModelEndpoints.ERNIE_Bot_Turbo.</param>
-        /// <returns>The configured MemoryClientBuilder instance.</returns>
+        /// <returns>The configured KernelMemoryBuilder instance.</returns>
         public static KernelMemoryBuilder WithERNIEBotTextGeneration(this KernelMemoryBuilder builder, ERNIEBotClient client, ModelEndpoint? endpoint = null)
         {
             builder.WithCustomTextGeneration(new ERNIEBotTextGeneration(client, endpoint));
@@ -27,12 +27,12 @@ namespace ERNIE_Bot.KernelMemory
         }
 
         /// <summary>
-        /// Configures the MemoryClientBuilder with ERNIEBot embedding generation.
+        /// Configures the KernelMemoryBuilder with ERNIEBot embedding generation.
         /// </summary>
-        /// <param name="builder">The MemoryClientBuilder instance to configure.</param>
+        /// <param name="builder">The KernelMemoryBuilder instance to configure.</param>
         /// <param name="client">The ERNIEBotClient instance to use for embedding generation.</param>
         /// <param name="endpoint">The endpoint to use for the embedding generation model. Defaults to ModelEndpoints.Embedding_v1.</param>
-        /// <returns>The configured MemoryClientBuilder instance.</returns>
+        /// <returns>The configured KernelMemoryBuilder instance.</returns>
         public static KernelMemoryBuilder WithERNIEBotEmbeddingGeneration(this KernelMemoryBuilder builder, ERNIEBotClient client, EmbeddingModelEndpoint? endpoint = null)
         {
             builder.WithCustomEmbeddingGeneration(new ERNIEBotTextEmbeddingGeneration(client, endpoint));
@@ -40,12 +40,12 @@ namespace ERNIE_Bot.KernelMemory
         }
 
         /// <summary>
-        /// Configures the MemoryClientBuilder with ERNIEBot defaults.
+        /// Configures the KernelMemoryBuilder with ERNIEBot defaults.
         /// </summary>
-        /// <param name="builder">The MemoryClientBuilder instance to configure.</param>
+        /// <param name="builder">The KernelMemoryBuilder instance to configure.</param>
         /// <param name="clientId">The client ID to use for the ERNIEBotClient instance.</param>
         /// <param name="clientSecret">The client secret to use for the ERNIEBotClient instance.</param>
-        /// <returns>The configured MemoryClientBuilder instance.</returns>
+        /// <returns>The configured KernelMemoryBuilder instance.</returns>
         public static KernelMemoryBuilder WithERNIEBotDefaults(this KernelMemoryBuilder builder, string clientId, string clientSecret)
         {
             var client = new ERNIEBotClient(clientId, clientSecret);
@@ -53,11 +53,11 @@ namespace ERNIE_Bot.KernelMemory
         }
 
         /// <summary>
-        /// Configures the MemoryClientBuilder with ERNIEBot defaults.
+        /// Configures the KernelMemoryBuilder with ERNIEBot defaults.
         /// </summary>
-        /// <param name="builder">The MemoryClientBuilder instance to configure.</param>
+        /// <param name="builder">The KernelMemoryBuilder instance to configure.</param>
         /// <param name="client">The ERNIEBotClient instance to use for text and embedding generation.</param>
-        /// <returns>The configured MemoryClientBuilder instance.</returns>
+        /// <returns>The configured KernelMemoryBuilder instance.</returns>
         public static KernelMemoryBuilder WithERNIEBotDefaults(this KernelMemoryBuilder builder, ERNIEBotClient client)
         {
             builder.WithERNIEBotTextGeneration(client);
