0e96c21b37a2b67a537d5b90d979717d26752a74
xbotter <xbotter@live.cn>
2023-10-30 17:05:47 +0800

    fix typo

--- samples/ERNIE-Bot-Kernel-Memory.Sample/Program.cs
+++ samples/ERNIE-Bot-Kernel-Memory.Sample/Program.cs
@@ -9,6 +9,8 @@ var config = new ConfigurationBuilder()
             .AddUserSecrets<Program>()
             .Build();
 
+var httpClient = new HttpClient();
+
 var memory = new MemoryClientBuilder()
         .WithERNIEBotDefaults(config["ClientId"]!, config["ClientSecret"]!)
         .With(new TextPartitioningOptions
