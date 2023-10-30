f3ca395722588c4b62f31099a210111f0e64ee65
xbotter <xbotter@users.noreply.github.com>
2023-10-30 19:43:37 +0800

    📝 Add ERNIE-Bot Kernel Memory  (#55)
    * 📝 Add ERNIE-Bot Kernel Memory sample and project files
    
    - Added a new project "ERNIE-Bot-Kernel-Memoy.Sample" to the solution.
    - The project is an executable project targeting .NET 6.0.
    - It references the "ERNIE-Bot.KernelMemory" project.
    - The project includes a sample PDF file "sample-SK-Readme.pdf" to be imported into the memory.
    - The "Program.cs" file contains code to ask a question to the memory and display the answer.
    - The code also displays the relevant sources for the answer.
    
    This commit adds a new sample project to demonstrate the usage of the ERNIE-Bot Kernel Memory.
    
    * fix typo
    
    * clean program
    
    * update package to KernelMemory
    
    * use DelayHttpHandler to avoid QPS error
    
    * MemoryClientBuilder -> KernelMemoryBuilder

---
+++ src/ERNIE-Bot.KernelMemory/ERNIEBotTextGeneration.cs
@@ -0,0 +1,52 @@
+﻿using ERNIE_Bot.SDK;
+using Microsoft.KernelMemory.AI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ERNIE_Bot.KernelMemory
+{
+    /// <summary>
+    /// Generating text using ERNIEBotClient.
+    /// </summary>
+    public class ERNIEBotTextGeneration : ITextGeneration
+    {
+        private readonly ERNIEBotClient _client;
+        private readonly ModelEndpoint _endpoint;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ERNIEBotTextGeneration"/> class.
+        /// </summary>
+        /// <param name="client">The ERNIEBotClient instance to use for generating text.</param>
+        /// <param name="endpoint">The endpoint to use for the model. Defaults to ModelEndpoints.ERNIE_Bot_Turbo.</param>
+        public ERNIEBotTextGeneration(ERNIEBotClient client, ModelEndpoint? endpoint = null)
+        {
+            this._client = client;
+            _endpoint = endpoint ?? ModelEndpoints.ERNIE_Bot_Turbo;
+        }
+
+        /// <inheritdoc/>
+        public async IAsyncEnumerable<string> GenerateTextAsync(string prompt, TextGenerationOptions options, [EnumeratorCancellation] CancellationToken cancellationToken = default)
+        {
+            var result = _client.ChatStreamAsync(new SDK.Models.ChatRequest()
+            {
+                Messages = new List<SDK.Models.Message>()
+                 {
+                     new SDK.Models.Message()
+                     {
+                          Role = SDK.Models.MessageRole.User,
+                          Content = prompt
+                     }
+                 },
+            }, _endpoint, cancellationToken);
+
+            await foreach (var response in result)
+            {
+                yield return response.Result;
+            }
+        }
+    }
+}
