390fadb517653a8dcfb8d9e06dadbe354ba59bd8
xbotter <xbotter@live.cn>
2023-11-09 12:37:07 +0800

    📝 Add Tokenizer class for text tokenization
    Add a new Tokenizer class in the ERNIE-Bot.SDK namespace that provides methods for tokenizing text. The class includes a method, ApproxNumTokens, which calculates the approximate number of tokens in a given text. The method counts the number of Han characters and estimates the number of words based on whitespace. The result is the sum of the Han character count and 1.3 times the word count. The class also includes a unit test for the ApproxNumTokens method.

---
+++ src/ERNIE-Bot.SDK/Tokenizer.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace ERNIE_Bot.SDK
+{
+    /// <summary>
+    /// This class provides methods for tokenizing text.
+    /// </summary>
+    public static class Tokenizer
+    {
+        public static int ApproxNumTokens(string text)
+        {
+            var hanCount = 0;
+            var res = new StringBuilder(text.Length);
+
+            foreach (var c in text)
+            {
+                if (char.IsWhiteSpace(c))
+                {
+                    res.Append(' ');
+                }
+                else if (char.GetUnicodeCategory(c) == System.Globalization.UnicodeCategory.OtherLetter)
+                {
+                    hanCount++;
+                    res.Append(' ');
+                }
+                else
+                {
+                    res.Append(c);
+                }
+            }
+
+            var wordCount = res.ToString().Split(' ', StringSplitOptions.RemoveEmptyEntries).Length;
+            return hanCount + (int)Math.Floor(wordCount * 1.3);
+        }
+    }
+}
---
+++ tests/ERNIE-Bot.SDK.Tests/TokenizerTests.cs
@@ -0,0 +1,16 @@
+using ERNIE_Bot.SDK;
+
+namespace ERNIE_Bot.SDK.Tests
+{
+    public class TokenizerTests
+    {
+        [Fact]
+        public void TestApproxNumTokens()
+        {
+            string text = "这是一段测试文字This is a test string.";
+            int expected = 14;
+            int actual = Tokenizer.ApproxNumTokens(text);
+            Assert.Equal(expected, actual);
+        }
+    }
+}
